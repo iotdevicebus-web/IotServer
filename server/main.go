be9decbcf120f60b7b1cd9aa47a9e195c0e0010d
IoT Platform Admin <admin@iot-platform.local>
2026-08-17 14:25:45 +0900

    docs: Add detailed Protocol Specification HTML document and server routing

--- server/main.go
+++ server/main.go
@@ -60,14 +60,18 @@ func main() {
 	fs := http.FileServer(http.Dir("./web"))
 	webMux.Handle("/", fs)
 
-	// システム総合仕様書 & 取扱説明書 & 画像ファイル配信
+	// システム総合仕様書 & 取扱説明書 & プロトコル仕様書 & 画像ファイル配信
 	webMux.HandleFunc("/spec", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../docs/system_specification.html")
 	})
 	webMux.HandleFunc("/guide", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../docs/server_user_guide.html")
 	})
+	webMux.HandleFunc("/protocol", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../docs/protocol_specification.html")
+	})
 	webMux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("../docs/images"))))
+
 
 
 
