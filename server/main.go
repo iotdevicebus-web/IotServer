9350c65764ff34fb01b40bf509ea139899d20aa2
IoT Platform Admin <admin@iot-platform.local>
2026-08-17 14:32:00 +0900

    docs: Add ESP32-S3 Edge User Guide HTML and update server routing

--- server/main.go
+++ server/main.go
@@ -60,7 +60,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./web"))
 	webMux.Handle("/", fs)
 
-	// システム総合仕様書 & 取扱説明書 & プロトコル仕様書 & STM32説明書 & 画像ファイル配信
+	// システム総合仕様書 & 取扱説明書 & プロトコル仕様書 & STM32説明書 & ESP32説明書 & 画像ファイル配信
 	webMux.HandleFunc("/spec", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../docs/system_specification.html")
 	})
@@ -73,7 +73,11 @@ func main() {
 	webMux.HandleFunc("/stm32", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "../docs/edge_user_guide_stm32.html")
 	})
+	webMux.HandleFunc("/esp32", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "../docs/edge_user_guide_esp32.html")
+	})
 	webMux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("../docs/images"))))
+
 
 
 
